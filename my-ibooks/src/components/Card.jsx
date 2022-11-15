524d18ff604aa741a842842463330c07dd7e712d
Ferruci <guferruci@gmail.com>
2022-11-14 23:03:41 -0300

    card e nav feitos

--- my-ibooks/src/components/Card.jsx
+++ my-ibooks/src/components/Card.jsx
@@ -1,8 +1,110 @@
 import React from "react"
 import "../assets/css/style.css"
 
+import Button from './Button';
+
 export default props =>
-    <div>
-        <img src = ""></img>
-        
+    <div className="min-w-[85%] max-h-300px items-center content-center justify-center mb-2">
+        <div href="#" class="flex-col flex w-full content-center bg-white rounded-lg border shadow-md sm:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            
+            {/* imagem */}
+            <img className = "object-cover m-1 max-h-64 w-80 h-80  flex rounded md:h-auto md:w-48 shadow-2xl invisible sm:visible" src={props.img} alt="Capa do Livro"/>
+
+            {/* dados */}
+            <div className = "w-full flex-wrap ml-7 ">
+                {/* titulo */}
+                <div className = "flex mt-3 mb-4 flex text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <h1>{props.titulo}</h1>
+                </div>
+
+                {/* dados */}
+                <div className = "flex flex-wrap mt-4 mb-0 content-center text-center items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <div className = "mr-3 items-center">
+                        <h2>Exemplares</h2>
+                        <h2>{props.exemplares}</h2>
+                    </div>
+                    <div className = "mr-3 items-center">
+                        <h2>Disponiveis</h2>
+                        <h2>{props.disponiveis}</h2>
+                    </div>
+                    <div className = "mr-3 items-center">
+                        <h2>Ocupados</h2>
+                        <h2>{props.emprestados}</h2>
+                    </div>
+                </div>
+                
+                {/* icons */}
+                <div className = "flex flex-wrap mt-14 mb-0 text-center items-center text-1xl font-bold tracking-tight text-gray-900 dark:text-white align-bottom">
+                    <div className = "flex-wrap justify-center w-40">
+                        <div className="flex justify-center">
+                            <p>Numero de Paginas</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{<FaBook/>}</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{props.tPaginas} Paginas</p>
+                        </div>
+                    </div>
+
+                    <div className = "flex-wrap justify-center w-40">
+                        <div className="flex justify-center">
+                            <p>Idioma</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{<FaGlobe/>}</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{props.idioma}</p>
+                        </div>
+                    </div>
+
+                    <div className = "flex-wrap justify-center w-40">
+                        <div className="flex justify-center">
+                            <p>Editora</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{<FaBuilding/>}</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{props.editora}</p>
+                        </div>
+                    </div>
+
+                    <div className = "flex-wrap justify-center w-40">
+                        <div className="flex justify-center">
+                            <p>ISBN-10</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{<FaQrcode/>}</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{props.isbn10}</p>
+                        </div>
+                    </div>
+
+                    <div className = "flex-wrap justify-center w-40">
+                        <div className=" flex justify-center">
+                            <p>ISBN-13</p>
+                        </div>
+                        <div className=" flex justify-center">
+                            <p>{<FaQrcode/>}</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <p>{props.isbn13}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* botoes de alterar */}
+            <div className = "flex w-12 h-12 mt-5 mr-3">
+                <Button icone={<FaPen/>} texto ="" bgColor="#3F00A6" tColor="#fff"/>
+            </div>
+
+            {/* botao de excluir */}
+            <div className = "flex w-12 h-12 mt-5 mr-5">
+                <Button icone={<FaTrash/>} texto ="" bgColor="#CE0000" tColor="#fff"/>
+            </div>
+        </div>
     </div>
--- my-ibooks/src/pages/livros/Livros.jsx
+++ my-ibooks/src/pages/livros/Livros.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import { FaBeer, FaPlus } from 'react-icons/fa';
+import { FaBook, FaBuilding, FaGlobe, FaPen, FaPlus, FaQrcode, FaTrash } from 'react-icons/fa';
 import Button from '../../components/Button';
+import Card from '../../components/Card';
+import Nav from '../../components/Nav';
 
 
 function Livro() {
@@ -8,15 +10,43 @@ function Livro() {
 
   return (
     <>
-    <header>
-      
-    </header>
-    <main>
+      {/* <div className='mb-5'>
+        <Nav/>
+      </div> */}
       <div>
-        <Button icone={<FaPlus/>} texto ="sei la" bgColor="red" tColor="#000"/>
-      </div>
-    </main>
-    
+        <Button icone={<FaTrash/>} texto ="" bgColor="#CE0000" tColor="#fff"/>
+      </div> 
+      {/* <div className='justify-center flex mt-10'>
+        <ul>
+          <li>
+            <Card titulo="Hunter x Hunter - 11"
+              exemplares="20"
+              emprestados="5"
+              disponiveis="15"
+              tPaginas="200"
+              idioma="Portugues"
+              editora="JBC"
+              isbn10="101010"
+              isbn13="131313"
+              img="./src/assets/images/capa.jpg">
+            </Card>
+          </li>
+          <li>
+            <Card titulo="Berserk - 27"
+              exemplares="20"
+              emprestados="5"
+              disponiveis="15"
+              tPaginas="216"
+              idioma="Portugues"
+              editora="Panini"
+              isbn10="8542612272"
+              isbn13="978-8542612271"
+              img="./src/assets/images/capaBerserk.jpg">
+            </Card>
+          </li>
+        </ul>
+      </div> */}
+
     </>
   )
 }
